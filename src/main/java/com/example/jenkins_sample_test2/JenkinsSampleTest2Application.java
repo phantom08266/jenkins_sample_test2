b0508595181e174e53392f27dbea3d9859760c8d
phantom08266 <phantom08266@gmail.com>
2023-04-09 22:10:59 +0900

    최초커밋

---
+++ src/main/java/com/example/jenkins_sample_test2/JenkinsSampleTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_sample_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSampleTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSampleTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins_sample_test2/JenkinsSampleTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_sample_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSampleTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
